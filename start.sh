9e83eb15ee664598f1699d9ec86723759ddac853
Victor Maina <contact@victormaina.com>
2020-10-20 07:01:26 +0300

    add source template with routing

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY="8ce22389489e4d22b88636929f09c14b"
+export NEWS_API_KEY="ae5667bec2624df6a8c6e3c8e6b793c0"
 export SECRET_KEY="<Flask WTF Secret Key>"
 
 python3 manage.py server
